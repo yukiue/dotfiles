41c3f6b4266d0a1d23559e8ae99433f2a97166ca
yukiue <45696312+yukiue@users.noreply.github.com>
2019-08-16 21:49:06 +0900

    add function.zsh

---
+++ .zsh/function.zsh
@@ -0,0 +1,16 @@
+function history-all { history -E 1 }
+
+function ghq-fzf() {
+    local selected_dir=$(ghq list | fzf --query="$LBUFFER")
+
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd $(ghq root)/${selected_dir}"
+        zle accept-line
+    fi
+
+    zle reset-prompt
+}
+
+zle -N ghq-fzf
+
+bindkey "^g" ghq-fzf
