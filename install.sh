74031e57adfd906fb8db86714ba27109eb2ec80e
yukiue <45696312+yukiue@users.noreply.github.com>
2019-08-31 15:19:38 +0900

    exclude .gitconfig from for statement

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTPATH=~/.dotfiles
 for dotfile in .??*
 do
     [ "$dotfile" = ".git" ] && continue
+    [ "$dotfile" = ".gitconfig" ] && continue
 
     ln -snfv "$DOTPATH/$dotfile" "$HOME"/"$dotfile"
 done
