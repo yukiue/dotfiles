7858845430e1628b2787704069cf2e5170a8e3f2
yukiue <45696312+yukiue@users.noreply.github.com>
2019-09-07 16:52:37 +0900

    remove .gitconfig

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ DOTPATH=~/.dotfiles
 for dotfile in .??*
 do
     [ "$dotfile" = ".git" ] && continue
-    [ "$dotfile" = ".gitconfig" ] && continue
 
     ln -snfv "$DOTPATH/$dotfile" "$HOME"/"$dotfile"
 done
