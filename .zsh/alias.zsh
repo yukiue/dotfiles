7cfba6fdeb9ce9e0ed6db71df04bce3f48ece71b
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-17 14:34:01 +0900

    organize aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,3 +1,4 @@
+# ls
 alias l='ls --color=auto'
 alias ls='ls --color=auto'
 alias la='ls -a'
@@ -5,43 +6,52 @@ alias ll='ls -l'
 alias lsf='ls -lF | grep ^-'
 alias lsd='ls -lF | grep ^d'
 
+
+# cd
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ~='cd ~'
+alias cdl='cd $(ls -lt | grep ^d | awk '\''NR==1 {print $9}'\'')'
 
+
+# shortcut
+alias g='git'
+alias x='startx'
+alias open='xdg-open'
+
+
+# basic alias
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -i'
 alias grep='grep --color=auto'
 alias mkdir='mkdir -p'
+alias reload='exec $SHELL -l'
+
+
+# xsel
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 
-alias e='emacs'
-alias g='git'
-alias x='startx'
-alias open='xdg-open'
 
 # emacs
-alias remacs='emacs -q'
-# alias remacs='emacs -q -l ~/.remacs.d'
-alias ec='emacsclient -e "(elscreen-create)" > /dev/null && emacsclient -n'
-alias ecn='emacsclient -n'
-alias e='emacs -nw'
-# alias mew='emacs -e mew'
+alias ec='emacsclient'
+alias enw='emacs -nw'
+alias oemacs='emacs -q'
+# alias oemacs='emacs -q -l ~/.remacs.d'
+alias ecn='emacsclient -e "(elscreen-create)" > /dev/null && emacsclient -n'
+alias e='(){pidof emacs > /dev/null && ecn $1 || emacs $1 &}'
 alias mew='emacsclient -e "(elscreen-create)" && emacsclient -e "(mew)"'
-# alias cde='''cd $(emacsclient -e "(return-current-working-directory-to-shell)" | sed 's/^"\(.*\)"$/\1/')'''
-
 alias cde='cd $(emacsclient -e "(return-current-working-directory-to-shell)" | sed s/\"//g)'
 
-alias reload='exec $SHELL -l'
-alias cdl='cd $(ls -lt | grep ^d | awk '\''NR==1 {print $9}'\'')'
+
+# xrandr
 alias xrandr_1920x1080_HDMI='xrandr --output HDMI-1 --same-as eDP-1 --mode 1920x1080'
-alias xrandr_800x600_HDMI='xrandr --output HDMI-1 --same-as eDP-1 --mode 800x600'
 alias xrandr_1920x1080_DP='xrandr --output DP-1 --same-as eDP-1 --mode 1920x1080'
-alias xrandr_800x600_DP='xrandr --output DP-1 --same-as eDP-1 --mode 800x600'
+
+
+# misc
 alias mozc='/usr/lib/mozc/mozc_tool --mode=config_dialog'
-alias ee='(){pidof emacs && ec $1 || emacs $1 &}'
 alias mupdf='(){mupdf $1 2> /dev/null}'
