a26eb0dbe8176e7b9481e160039efe14f3b54790
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-17 13:51:48 +0900

    add alias to execute emacsclient if emacs is running, otherwise execute emacs

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -43,3 +43,4 @@ alias xrandr_800x600_HDMI='xrandr --output HDMI-1 --same-as eDP-1 --mode 800x600
 alias xrandr_1920x1080_DP='xrandr --output DP-1 --same-as eDP-1 --mode 1920x1080'
 alias xrandr_800x600_DP='xrandr --output DP-1 --same-as eDP-1 --mode 800x600'
 alias mozc='/usr/lib/mozc/mozc_tool --mode=config_dialog'
+alias ee='(){pidof emacs && ec $1 || emacs $1 &}'
