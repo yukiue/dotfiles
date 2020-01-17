29ca0833b796d48ddd57aa49d2c36005ed2cf3c0
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-17 15:10:18 +0900

    move several lines from .zshrc and comment out ghq-fzf

--- .zsh/function.zsh
+++ .zsh/function.zsh
@@ -1,16 +1,18 @@
 function history-all { history -E 1 }
 
-function ghq-fzf() {
-    local selected_dir=$(ghq list | fzf --query="$LBUFFER")
+# function ghq-fzf() {
+#     local selected_dir=$(ghq list | fzf --query="$LBUFFER")
 
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd $(ghq root)/${selected_dir}"
-        zle accept-line
-    fi
+#     if [ -n "$selected_dir" ]; then
+#         BUFFER="cd $(ghq root)/${selected_dir}"
+#         zle accept-line
+#     fi
 
-    zle reset-prompt
-}
+#     zle reset-prompt
+# }
 
-zle -N ghq-fzf
+# zle -N ghq-fzf
 
-bindkey "^g" ghq-fzf
+# bindkey "^g" ghq-fzf
+
+function chpwd() { ls }
