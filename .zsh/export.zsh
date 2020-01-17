d78ed0998cc2f7a07fdddd8c93d4dd3397273a23
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-17 15:08:11 +0900

    move several lines from .zshrc

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -2,3 +2,12 @@ export GOPATH="$HOME/go"
 export PATH="$PATH:$HOME/go/bin"
 export PATH="$PATH:$HOME/bin"
 export PYTHONPATH="$HOME/lib/python"
+
+export LSCOLORS=exfxcxdxbxegedabagacad
+export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
+
+export HISTFILE=~/.zsh_history
+export HISTSIZE=1000
+export SAVEHIST=100000
+
+export TEXINPUTS=.:~/lib/texmf//:/usr/share/texlive/texmf-dist//:/etc/texmf//
