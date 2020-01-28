12c61d286c128e81c54051a9f153d15e8d3deb35
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-28 13:12:40 +0900

    uncomment

--- .zsh/proxy.zsh
+++ .zsh/proxy.zsh
@@ -1,25 +1,25 @@
 # PROXY=http://proxy.ksc.kwansei.ac.jp:80803
 
-# kgproxy(){
-#     export http_proxy=http://proxy.ksc.kwansei.ac.jp:8080
-#     export https_proxy=http://proxy.ksc.kwansei.ac.jp:8080
-#     export ftp_proxy=http://proxy.ksc.kwansei.ac.jp:8080
-# }
+kgproxy(){
+    export http_proxy=http://proxy.ksc.kwansei.ac.jp:8080
+    export https_proxy=http://proxy.ksc.kwansei.ac.jp:8080
+    export ftp_proxy=http://proxy.ksc.kwansei.ac.jp:8080
+}
 
-# unproxy(){
-#     unset http_proxy
-#     unset https_proxy
-#     unset ftp_proxy
-# }
+unproxy(){
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+}
 
-# PROXY=http://proxy.ksc.kwansei.ac.jp:8080
+PROXY=http://proxy.ksc.kwansei.ac.jp:8080
 
-# if /sbin/iwconfig wlp2s0 | grep -o "lsnl" >/dev/null ; then
-#     export http_proxy=$PROXY
-#     export https_proxy=$PROXY
-#     export ftp_proxy=$PROXY
-# else
-#     unset http_proxy
-#     unset https_proxy
-#     unset ftp_proxy
-# fi
+if /sbin/iwconfig wlp2s0 | grep -o "lsnl" >/dev/null ; then
+    export http_proxy=$PROXY
+    export https_proxy=$PROXY
+    export ftp_proxy=$PROXY
+else
+    unset http_proxy
+    unset https_proxy
+    unset ftp_proxy
+fi
