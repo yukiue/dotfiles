2b906bc4297698a009e10870d11b190b6160d9f6
yukiue <45696312+yukiue@users.noreply.github.com>
2020-01-30 22:11:07 +0900

    add alias to change character code

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -56,3 +56,4 @@ alias xrandr_1920x1080_DP='xrandr --output DP-1 --same-as eDP-1 --mode 1920x1080
 alias mozc='/usr/lib/mozc/mozc_tool --mode=config_dialog'
 alias mupdf='(){mupdf $1 2> /dev/null}'
 alias chromium='(){chromium $1 2> /dev/null}'
+alias utf8='nkf -w --overwrite'
