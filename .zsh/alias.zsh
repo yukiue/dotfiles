521e4322e65ecc6a1362b34b92d918995924e139
yukiue <45696312+yukiue@users.noreply.github.com>
2020-02-18 02:33:29 +0900

    add several alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,7 +1,7 @@
 # ls
 alias l='ls --color=auto'
 alias ls='ls --color=auto'
-alias la='ls -a'
+alias la='ls -A'
 alias ll='ls -lh'
 alias lsf='ls -lF | grep ^-'
 alias lsd='ls -lF | grep ^d'
@@ -38,7 +38,7 @@ alias pbpaste='xsel --clipboard --output'
 
 # emacs
 alias ec='emacsclient'
-alias enw='emacs -nw'
+alias ee='emacs -nw'
 # alias oemacs='emacs -q'
 alias oemacs='emacs -q -l ~/.emacs.conf'
 alias en='emacsclient -n'
@@ -56,5 +56,7 @@ alias xrandr_1920x1080_DP='xrandr --output DP-1 --same-as eDP-1 --mode 1920x1080
 # misc
 alias mozc='/usr/lib/mozc/mozc_tool --mode=config_dialog'
 alias mupdf='(){mupdf $1 2> /dev/null}'
-alias chromium='(){chromium $1 2> /dev/null}'
+# alias chromium='(){chromium $1 2> /dev/null }'
 alias utf8='nkf -w --overwrite'
+alias range='(){head -$2 | tail -`expr $2 - $1 + 1`}'
+alias localhost='python -m http.server'
