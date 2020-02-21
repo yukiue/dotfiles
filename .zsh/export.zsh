06d58f37a619aef160028a52e101d55eee5941fe
yukiue <45696312+yukiue@users.noreply.github.com>
2020-02-21 16:57:37 +0900

    change HISTSIZE and SAVEHIST

--- .zsh/export.zsh
+++ .zsh/export.zsh
@@ -7,7 +7,7 @@ export LSCOLORS=exfxcxdxbxegedabagacad
 export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=46;34:cd=43;34:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 
 export HISTFILE=~/.zsh_history
-export HISTSIZE=1000
-export SAVEHIST=100000
+export HISTSIZE=10000
+export SAVEHIST=1000000
 
 export TEXINPUTS=.:~/lib/texmf//:/usr/share/texlive/texmf-dist//:/etc/texmf//
