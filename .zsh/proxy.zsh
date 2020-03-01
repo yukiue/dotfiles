0ec42a66e1858c810f497249f526192b29c51a9f
yukiue <45696312+yukiue@users.noreply.github.com>
2020-03-01 20:24:48 +0900

    remove unnecessary settings

--- .zsh/proxy.zsh
+++ .zsh/proxy.zsh
@@ -9,15 +9,3 @@ unproxy(){
     unset https_proxy
     unset ftp_proxy
 }
-
-PROXY=http://proxy.ksc.kwansei.ac.jp:8080
-
-# if /sbin/iwconfig wlp2s0 | grep -o "lsnl" >/dev/null ; then
-#     export http_proxy=$PROXY
-#     export https_proxy=$PROXY
-#     export ftp_proxy=$PROXY
-# else
-#     unset http_proxy
-#     unset https_proxy
-#     unset ftp_proxy
-# fi
