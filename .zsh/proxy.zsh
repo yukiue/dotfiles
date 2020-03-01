18941438cf28f034ef4e10abbde9939a4456f663
yukiue <45696312+yukiue@users.noreply.github.com>
2020-03-01 16:30:07 +0900

    comment out auto-proxy-settings

--- .zsh/proxy.zsh
+++ .zsh/proxy.zsh
@@ -1,5 +1,3 @@
-# PROXY=http://proxy.ksc.kwansei.ac.jp:80803
-
 kgproxy(){
     export http_proxy=http://proxy.ksc.kwansei.ac.jp:8080
     export https_proxy=http://proxy.ksc.kwansei.ac.jp:8080
@@ -14,12 +12,12 @@ unproxy(){
 
 PROXY=http://proxy.ksc.kwansei.ac.jp:8080
 
-if /sbin/iwconfig wlp2s0 | grep -o "lsnl" >/dev/null ; then
-    export http_proxy=$PROXY
-    export https_proxy=$PROXY
-    export ftp_proxy=$PROXY
-else
-    unset http_proxy
-    unset https_proxy
-    unset ftp_proxy
-fi
+# if /sbin/iwconfig wlp2s0 | grep -o "lsnl" >/dev/null ; then
+#     export http_proxy=$PROXY
+#     export https_proxy=$PROXY
+#     export ftp_proxy=$PROXY
+# else
+#     unset http_proxy
+#     unset https_proxy
+#     unset ftp_proxy
+# fi
