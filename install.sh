bbf589dda788f156fb96dbd04ec3e5663bbbb94d
yukiue <45696312+yukiue@users.noreply.github.com>
2020-03-21 16:51:47 +0900

    update

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 DOTPATH=~/.dotfiles
 
-for dotfile in .??*
+for dotfile in `find . -mindepth 1 -maxdepth 1 -name ".*"  -exec basename {} \;`
 do
     [ "$dotfile" = ".git" ] && continue
+    [ "$dotfile" = ".config" ] && continue
+    ln -snfv "$DOTPATH/$dotfile" "$HOME/$dotfile"
+done
 
-    ln -snfv "$DOTPATH/$dotfile" "$HOME"/"$dotfile"
+mkdir -p ~/.config
+
+for dotfile in `find ./.config -mindepth 1 -maxdepth 1 -exec basename {} \;`
+do
+    ln -snfv "$DOTPATH/.config/$dotfile" "$HOME/.config/$dotfile"
 done
